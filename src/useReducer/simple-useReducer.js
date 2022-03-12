4dc464273c4027801cdd3c2859fd60eb991254e1
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-12 22:10:56 +0600

    re practice

--- src/useReducer/simple-useReducer.js
+++ src/useReducer/simple-useReducer.js
@@ -1,15 +1,27 @@
 import React, { useReducer } from "react";
 
 
-
+const initialValue = 0;
+const reducer=(state,action)=>{
+  switch(action){
+      case 'increment':
+          return state + 1;
+      case 'decrement' :
+          return state - 1;
+      default :
+          return state;        
+  }
+}
 
 const SimpleUseReducer=()=>{
-
+ const [count, dispatch]  = useReducer(reducer,initialValue)
 
 return(
     <>
     <h2>hhhhh</h2>
-  
+  <p>{count}</p>
+  <button onClick={()=>{dispatch('increment')}}>increment</button>
+  <button onClick={()=>{dispatch('decrement')}}>decrement</button>
     
     </>
 )
